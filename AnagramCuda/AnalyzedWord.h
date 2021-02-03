134d366fc13b58de29ced8072c2c27315eb98926
Ruttmann <matthias@ruttmann.name>
2021-02-03 11:41:44 +0100

    move types

---
+++ AnagramCuda/AnalyzedWord.h
@@ -0,0 +1,140 @@
+#pragma once
+
+/// <summary>
+/// Number of possible input chars. 0-9 and A-Z
+/// </summary>
+constexpr uint32_t possibleCharacterCount = 36;
+
+/// <summary>
+/// type to count the occurences per character
+/// </summary>
+using usedCharacterCount_t = int8_t;
+
+/// <summary>
+/// type for the mask of existing characters
+/// </summary>
+using usedCharacterMask_t = uint32_t;
+
+struct AnalyzedWord
+{
+    /// <summary>
+    /// The actually used characters as bitmask
+    /// </summary>
+    usedCharacterMask_t usedMask;
+
+    /// <summary>
+    /// The mask for all characters that have at least one character left
+    /// </summary>
+    usedCharacterMask_t remainingMask;
+
+    /// <summary>
+    /// The usages per character
+    /// </summary>
+    usedCharacterCount_t counts[possibleCharacterCount];
+
+    /// <summary>
+    /// The id of the word
+    /// </summary>
+    int32_t wordId;
+
+    /// <summary>
+    /// the length of the word
+    /// </summary>
+    size_t length;
+
+    /// <summary>
+    /// the length until the anagram is fullfilled
+    /// </summary>
+    size_t restLength;
+
+    /// <summary>
+    /// The remaining count per character to fullfill the anagram
+    /// </summary>
+    usedCharacterCount_t remaining[possibleCharacterCount];
+
+    /// <summary>
+    /// Check these character indices (the rest is covered by the mask)
+    /// </summary>
+    int checkCharacters[possibleCharacterCount];
+
+    int checkCharacterCount;
+
+    void initAnagram(const std::string& text)
+    {
+        memset(&counts, 0, sizeof(counts));
+        usedMask = 0;
+        length = text.size();
+        restLength = length;
+        checkCharacterCount = 0;
+
+        for (auto c : text)
+        {
+            if (isalnum(c))
+            {
+                auto ci = toIndex(c);
+                if (counts[ci] == 0)
+                {
+                    checkCharacters[checkCharacterCount++] = ci;
+                }
+
+                counts[ci]++;
+
+                auto cu = std::min(ci, (char)31);
+                usedMask |= 1 << cu;
+            }
+        }
+
+        memcpy(&remaining, counts, sizeof(counts));
+        remainingMask = usedMask;
+    }
+
+    bool initWord(const AnalyzedWord& anagram, const std::string& text)
+    {
+        memset(&counts, 0, sizeof(counts));
+        memcpy(&remaining, anagram.remaining, sizeof(remaining));
+        remainingMask = anagram.usedMask;
+        usedMask = 0;
+        checkCharacterCount = anagram.checkCharacterCount;
+        memcpy(checkCharacters, anagram.checkCharacters, sizeof(checkCharacters));
+        length = text.size();
+        restLength = anagram.length - length;
+
+        for (auto c : text)
+        {
+            auto ci = toIndex(c);
+            counts[ci]++;
+
+            if (remaining[ci] == 0)
+            {
+                return false;
+            }
+
+            usedMask |= 1 << (std::min(ci, (char)31));
+            if (remaining[ci] > 1)
+            {
+                remaining[ci]--;
+            }
+            else
+            {
+                remaining[ci] = 0;
+                if (ci < 31)
+                {
+                    remainingMask ^= 1 << ci;
+                }
+            }
+        }
+
+        return true;
+    }
+
+private:
+    inline char toIndex(char c)
+    {
+        if (c >= 'A')
+        {
+            return toupper(c) - 'A' + 10;
+        }
+
+        return c - '0';
+    }
+};
---
+++ AnagramCuda/PartialAnagram.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "AnalyzedWord.h"
+
+struct partialAnagramEntry
+{
+    partialAnagramEntry()
+    {
+    }
+
+    /// <summary>
+    /// Constructor from a singleWord
+    /// </summary>
+    /// <param name="word"></param>
+    partialAnagramEntry(const AnalyzedWord& word)
+    {
+        initFromAnalyzedWord(word);
+    }
+
+    /// <summary>
+    /// bitflags for all characters that have an expected count of 0
+    /// </summary>
+    usedCharacterMask_t doNotUseMask;
+
+    /// <summary>
+    /// Index of the previous entry for combinations. -1 for single words
+    /// </summary>
+    int32_t previousEntry;
+
+    /// <summary>
+    /// The id of the word in a global array
+    /// </summary>
+    int32_t wordId;
+
+    /// <summary>
+    /// the counts per character
+    /// </summary>
+    usedCharacterCount_t counts[possibleCharacterCount];
+
+    /// <summary>
+    /// total sum of characters
+    /// </summary>
+    int32_t restLength;
+
+    /// <summary>
+    /// copy data from an analysed word
+    /// </summary>
+    /// <param name="word">the word</param>
+    void initFromAnalyzedWord(const AnalyzedWord& word)
+    {
+        memcpy(&counts, word.remaining, sizeof(counts));
+        doNotUseMask = ~word.remainingMask;
+        wordId = word.wordId;
+        previousEntry = -1;
+    }
+
+    /// <summary>
+    /// join existing data with a new word
+    /// </summary>
+    void joinWord(const partialAnagramEntry& entry, const AnalyzedWord& word, int index)
+    {
+        previousEntry = index;
+        doNotUseMask = entry.doNotUseMask;
+        wordId = word.wordId;
+        for (int i = 0; i < possibleCharacterCount; i++)
+        {
+            auto count = entry.counts[i] - word.counts[i];
+            counts[i] = count;
+            if (count == 0)
+            {
+                doNotUseMask |= i < 32 ? (1 << i) : (1 << 31);
+            }
+        }
+    }
+};
+
